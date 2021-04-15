bc336e06dbad4c8f7648d055b29a0bdc1fb75df7
Ryan Panos <rpanos@salesforce.com>
2021-04-14 17:01:26 -0700

    Simplest test

--- force-app/main/default/lwc/recordViewForm/__tests__/recordViewFormSimple.spec.js
+++ force-app/main/default/lwc/recordViewForm/__tests__/recordViewFormSimple.spec.js
@@ -1,1 +1,26 @@
 import { createElement } from 'lwc';
+import MockRecordHolder from 'lightningtest/mockRecordEditHolder';
+
+jest.mock('lightning/uiRecordApi', () => {
+    const { createLdsTestWireAdapter } = require('@salesforce/wire-service-jest-util');
+    return {
+        getRecordUi: createLdsTestWireAdapter(jest.fn())
+    }
+});
+
+
+
+
+describe('record view form', () => {
+    it('wires recordUi to input fields', () => {
+        const element = createElement('lightningtest-mock-record-edit-holder', {
+            is: MockRecordHolder
+        });
+
+        element.recordId = DEFAULT_RECORD_ID;
+        element.objectApiName = 'Bad_Guy__c';
+        document.body.appendChild(element);
+    });
+
+
+});
